8b0865c6766f0a98b583ad725f5fd3dbb47a258c
Uian Sol <usoldeveloper@gmail.com>
2024-02-01 11:33:44 -0300

    feature: add read all to product repository

--- internal/application/interfaces/product_repository.go
+++ internal/application/interfaces/product_repository.go
@@ -5,6 +5,7 @@ import "github.com/uiansol/product-follow-up/internal/application/entities"
 type IProductRepository interface {
 	Save(user *entities.Product) (string, error)
 	Read(id string) (*entities.Product, error)
+	ReadAll() ([]*entities.Product, error)
 	Update(user *entities.Product) error
 	Delete(id string) error
 }
